c74f8e6e1f79a3af3850840c94ea26de66cd74a0
Javier Sanchez <jefe10jav@gmail.com>
2019-08-17 12:23:59 -0500

    Updates

---
+++ precache-manifest.1666b487dde2a0def7f02741e2b20b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca9b8d6822c442b4d5d680643c25dc7",
+    "url": "/jefe10jav/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0f02a3a40751464f5393",
+    "url": "/jefe10jav/tic-tac-toe/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8bed4d2de7f9c0513fe0",
+    "url": "/jefe10jav/tic-tac-toe/static/css/main.9705333d.chunk.css"
+  },
+  {
+    "revision": "0f02a3a40751464f5393",
+    "url": "/jefe10jav/tic-tac-toe/static/js/2.175d20a9.chunk.js"
+  },
+  {
+    "revision": "8bed4d2de7f9c0513fe0",
+    "url": "/jefe10jav/tic-tac-toe/static/js/main.a350eeac.chunk.js"
+  },
+  {
+    "revision": "3afc7c16b4e354009f2c",
+    "url": "/jefe10jav/tic-tac-toe/static/js/runtime~main.af827864.js"
+  }
+]);
